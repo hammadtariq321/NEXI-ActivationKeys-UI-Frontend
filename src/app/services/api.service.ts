d88b173fd93fd940cb197159bd93c18a9d899fe5
hammadtariq321 <hammadtariq321@gmail.com>
2022-09-23 02:17:21 -0700

    Now activation can be Edit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  api_url = environment.API_URL
+  api_url = 'https://nexsusactivation.herokuapp.com'
 
   constructor(private http: HttpClient) { }
 
