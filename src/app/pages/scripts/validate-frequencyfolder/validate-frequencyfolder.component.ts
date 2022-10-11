f5d3fc40a6c842eef64994833b37504c12cdc9e7
hammadtariq321 <hammadtariq321@gmail.com>
2022-10-11 02:41:54 -0700

    validate Freq

--- src/app/pages/scripts/validate-frequencyfolder/validate-frequencyfolder.component.ts
+++ src/app/pages/scripts/validate-frequencyfolder/validate-frequencyfolder.component.ts
@@ -24,23 +24,34 @@ export class ValidateFrequencyfolderComponent implements OnInit {
   }
   
   fileResponse(event: any) {
+    let freqArray: any[] = []
+    if (event.target.files.length > 0){
+      let files = event.target.files;
+      console.log(files)
+      for (let f of files) {
+        console.log(f.name)
+        freqArray.push(f.name)
+      }
+    }  
     this.fileselected = true
     this.spinner = true
-    let file = event.target.files[0]
-    console.log(file)
-    let formData = new FormData(); 
-    formData.append('file_uploaded', file); 
-    this.api.ValidateFrequencyFolder(formData).subscribe(
-      (res) => {
-        console.log(res)
-        this.spinner = false
-        this.response = res
-      }, 
-      (err)=> {
-        console.log(err)
-        this.spinner = false
-      }
-    )
+    // let file = event.target.files[0]
+    // console.log(file)
+    // let formData = new FormData(); 
+    // formData.append('file_uploaded', file); 
+    setTimeout(() => {
+      this.api.ValidateFrequencyFolder({'freq': freqArray}).subscribe(
+        (res) => {
+          console.log(res)
+          this.spinner = false
+          this.response = res
+        }, 
+        (err)=> {
+          console.log(err)
+          this.spinner = false
+        }
+      )
+    }, 8000);
   }
 
 }
