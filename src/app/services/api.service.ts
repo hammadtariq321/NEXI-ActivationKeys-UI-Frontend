6a22c7028acdb057e736490ace68c5ef83d7ff19
hammadtariq321 <hammadtariq321@gmail.com>
2023-02-27 14:45:34 +0500

    added license type with dropdowns

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,9 +9,9 @@ import { HttpClient, HttpHeaders  } from '@angular/common/http';
 })
 export class ApiService {
 
-  api_url = 'https://nexsusactivation.herokuapp.com'
+  // api_url = 'https://nexsusactivation.herokuapp.com'
   // api_url = 'https://nexsus-activation-backup.herokuapp.com'
-  // api_url = 'http://127.0.0.1:8000' 
+  api_url = 'http://127.0.0.1:8000' 
 
 
   constructor(private http: HttpClient) { }
